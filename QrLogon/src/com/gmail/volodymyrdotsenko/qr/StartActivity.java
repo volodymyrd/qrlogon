144c338370be00d2eb7a2acc1e1c8ea344203ed7
volodymyrdotsenko@gmail.com <volodymyrdotsenko@gmail.com>
2014-02-26 08:58:22 +0200

    scan

--- QrLogon/src/com/gmail/volodymyrdotsenko/qr/StartActivity.java
+++ QrLogon/src/com/gmail/volodymyrdotsenko/qr/StartActivity.java
@@ -2,7 +2,9 @@ package com.gmail.volodymyrdotsenko.qr;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class StartActivity extends Activity {
 
@@ -19,4 +21,21 @@ public class StartActivity extends Activity {
 		return true;
 	}
 
+	public void scan(View view) {
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		startActivityForResult(intent, 0);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				// Handle successful scan
+			} else if (resultCode == RESULT_CANCELED) {
+				// Handle cancel
+			}
+		}
+	}
 }
